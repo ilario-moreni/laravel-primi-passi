47dd2071b90941577a09c32de35d83bc3757726d
ilario-moreni <ilario2001.mor@gmail.com>
2023-02-15 16:08:09 +0100

    hello laravel

---
+++ resources/views/contatti.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <ul>
+        <div>
+            <a href="{{ route('home') }}">Torna alla home</a>
+        </div>
+        <li>
+        <li>Ilario Moreni</li>
+        <li>Studente Boolean</li>
+        <li>ilariomoreni@mail</li>
+        </li>
+    </ul>
+</body>
+
+</html>
